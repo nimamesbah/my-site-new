7c129311e7e7af3e95863afec12c00119320820c
nimamesbah <nimamesbah85@gmail.com>
2025-04-07 17:34:00 +0430

    desktop done

--- assets/js.js
+++ assets/js.js
@@ -1,13 +1,23 @@
 const skillLists = document.querySelectorAll("#skill-ul>li")
 const loading =document.getElementById("loading")
+const email = document.getElementById("email-div")
+const num = document.getElementById("num-div")
 loading.classList.remove("opacity-100")
 loading.classList.add("opacity-0")
+
 setTimeout(()=> loading.classList.add("hidden"),200)
 
 let delay = 300
 for(let i = 0 ; skillLists.length>i;i++){
-    delay+=100
+    delay+=150
     setTimeout(()=>{
         skillLists[i].classList.remove("translate-x-[-90px]")
     },delay)
 }
+ 
+setTimeout(() => {
+    num.classList.remove("translate-y-[-60px]")
+},400 );
+setTimeout(() => {
+    email.classList.remove("translate-y-[-60px]")
+},500 );
