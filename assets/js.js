5907573a178bb0f548904098ead2bfe5a43f4c80
nimamesbah <nimamesbah85@gmail.com>
2025-04-10 19:27:55 +0430

    structure for mobile done

--- assets/js.js
+++ assets/js.js
@@ -1,23 +1,36 @@
+const body = document.querySelector("body")
 const skillLists = document.querySelectorAll("#skill-ul>li")
-const loading =document.getElementById("loading")
+const loading = document.getElementById("loading")
 const email = document.getElementById("email-div")
 const num = document.getElementById("num-div")
-loading.classList.remove("opacity-100")
-loading.classList.add("opacity-0")
+const projectsMobile = document.getElementById("projects-mobile")
+console.log(screen.width)
 
-setTimeout(()=> loading.classList.add("hidden"),200)
+if (screen.width > 550) {
+    loading.classList.remove("opacity-100")
+    loading.classList.add("opacity-0")
+    setTimeout(() => loading.classList.add("hidden"), 200)
 
-let delay = 300
-for(let i = 0 ; skillLists.length>i;i++){
-    delay+=150
-    setTimeout(()=>{
-        skillLists[i].classList.remove("translate-x-[-90px]")
-    },delay)
+    let delay = 300
+    for (let i = 0; skillLists.length > i; i++) {
+        delay += 150
+        setTimeout(() => {
+            skillLists[i].classList.remove("translate-x-[-90px]")
+        }, delay)
+    }
+
+    setTimeout(() => {
+        num.classList.remove("translate-y-[-60px]")
+    }, 400);
+    setTimeout(() => {
+        email.classList.remove("translate-y-[-60px]")
+    }, 500);
 }
- 
-setTimeout(() => {
-    num.classList.remove("translate-y-[-60px]")
-},400 );
-setTimeout(() => {
-    email.classList.remove("translate-y-[-60px]")
-},500 );
+
+function mobileRender() {
+    if (screen.width < 550) {
+
+    }
+}
+window.addEventListener("resize", mobileRender)
+projectsMobile.scrollLeft = -42
